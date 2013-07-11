b66a1bfaff79b0f05e39e96fe4b207ba1242353d
pallavi <pallavi@pallavi-work.(none)>
2013-07-11 12:41:18 +0530

    Raw app added

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'd6034fb7d82d72bda0ff6b16012e9f595a194a82906fd2c5456394be480291b9f1d49d002cf1a095c379545be2d386b4e9d43a92ed3807e3925abbeb033038a4'
